4c7b8b2a226512008820d54dd3deba5c90b270fc
BradJones-3 <bradjones_95@hotmail.com>
2021-06-15 11:57:33 +0000

    changed speed of which the how to play instructions appear

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 /* Toggles text indide modal on how to play the game */
 $(document).ready(function(){
     $("#rules-btn").click(function(){
-      $(".game-content").toggle(550);
+      $(".game-content").toggle(750);
     });
   });
-  /* Game variables */
+/* Game variables */
 
 const cards = document.querySelectorAll('.game-cards');
 
